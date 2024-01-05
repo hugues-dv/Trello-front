4ae11ad079d852646d8f06fd60c0d06967aea0eb
hugues-dv <h2vregille@gmail.com>
2024-01-05 15:50:35 +0100

    update list

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
-import { List, ListsService } from '../../services/lists.service';
+import { List, ListService } from '../../services/lists.service';
 
 @Component({
   selector: 'app-list',
@@ -13,18 +13,20 @@ import { List, ListsService } from '../../services/lists.service';
 export class ListComponent implements OnInit {
   list: List = {
     id: 2,
-    nom: 'You',
+    name: 'You',
     idProject: 1,
   };
 
-  constructor(private listsService: ListsService) {}
+  constructor(private listsService: ListService) {}
 
   // Méthode pour effectuer une action lorsque le composant est initialisé
   ngOnInit() {
     // Exemple d'utilisation du service ListsService pour obtenir des données
-    this.listsService.getListeById(this.list.id).subscribe((data: List) => {
-      // Mettre à jour la propriété list avec les données récupérées
-      this.list = data;
-    });
+    this.listsService
+      .getListeById(Number(this.list.id))
+      .subscribe((data: List) => {
+        // Mettre à jour la propriété list avec les données récupérées
+        this.list = data;
+      });
   }
 }
