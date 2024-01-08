0024ee8571fd4ee4938f6a4ecfefc43b00601bbd
Alexandre BAUCHET <alex.bauchet95160@gmail.com>
2024-01-08 12:47:52 +0100

    push

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -11,22 +11,11 @@ import { List, ListService } from '../../services/lists.service';
   styleUrl: './list.component.scss',
 })
 export class ListComponent implements OnInit {
-  list: List = {
-    id: 2,
-    name: 'You',
-    idProject: 1,
-  };
-
   constructor(private listsService: ListService) {}
 
-  // Méthode pour effectuer une action lorsque le composant est initialisé
-  ngOnInit() {
-    // Exemple d'utilisation du service ListsService pour obtenir des données
-    this.listsService
-      .getListeById(Number(this.list.id))
-      .subscribe((data: List) => {
-        // Mettre à jour la propriété list avec les données récupérées
-        this.list = data;
-      });
-  }
+  ngOnInit() {}
+
+  addCard() {}
+
+  removeList() {}
 }
