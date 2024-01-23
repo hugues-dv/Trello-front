89daa78057c96560c8254060e7e13acce6e16d4e
Alexandre BAUCHET <alex.bauchet95160@gmail.com>
2024-01-23 15:25:00 +0100

    add html css auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { ListComponent } from './components/list/list.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ProjectsComponent, ListComponent],
+  imports: [CommonModule, RouterOutlet, ProjectsComponent, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'trello-front';
+  user = null;
 }
--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { User } from '../../services/users.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
 export class AuthComponent {
-  projects!: User;
+  username!: string;
+
+  signIn() {}
 }
